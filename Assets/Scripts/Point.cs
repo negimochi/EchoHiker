18d0176a7b5be1ca0a84b6b42591a7f8629fff13
negimochi <negimochi.tabetai@gmail.com>
2012-05-18 08:55:48 +0900

    Pointクラス追加

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Point
+{
+    [SerializeField]
+    private Vector2 rect;
+
+    public Point()
+    {
+        rect = new Vector2();
+    }
+    public Point(float x, float y)
+    {
+        rect = new Vector2( x, y );
+    }
+
+    public void Polar(float r, float theta)
+    {
+        rect.x = Mathf.Cos(theta);
+        rect.y = Mathf.Sin(theta);
+        rect *= r;
+    }
+
+    public void Rect(float x, float y)
+    {
+        rect.x = x;
+        rect.y = y;
+    }
+
+    public float x
+    {
+        get { return rect.x; }
+        set { rect.x = value; }
+    }
+    public float y
+    {
+        get { return this.rect.y; }
+        set { rect.y = value; }
+    }
+
+    public float r
+    {
+        get { return rect.magnitude; }
+        set { Polar(value, Mathf.Atan2(rect.y, rect.x)); }
+    }
+    public float theta
+    {
+        get { return Mathf.Atan2(rect.y, rect.x); }
+        set { Polar(rect.magnitude, value); }
+    }
+}
--- Assets/Scripts/RandomGenerator.cs
+++ Assets/Scripts/RandomGenerator.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class RandomEnemyGenerator : MonoBehaviour {
+public class RandomGenerator : MonoBehaviour {
 
     [SerializeField]
     private GameObject[] itemObject;
@@ -10,22 +10,26 @@ public class RandomEnemyGenerator : MonoBehaviour {
     [SerializeField]
     private int lines;
     [SerializeField]
-    private float insideRadius;
+    private float height;
     [SerializeField]
-    private float outsideRadius;
+    private Point inside;
+    [SerializeField]
+    private Point outside;
 
+    public Point side;
 
 	// Use this for initialization
 	void Start () {
         Random.seed = (int)(System.DateTime.Now.TimeOfDay.TotalMilliseconds);
         Debug.Log( "seed=" + Random.seed );
-
-        Vector3 sub = endPos - startPos;
-        for (int i = 0; i < size; i++)
-        {
+        Point sub = new Point();
+        sub.Polar(outside.r - inside.r, outside.theta - inside.r );
+        for (int i = 0; i < lines; i++) {
             // アイテムの種類、位置を決める
             int index = Random.Range(0, itemObject.Length - 1);
-            Vector3 pos = new Vector3(Random.value * sub.x + startPos.x, Random.value * sub.y + startPos.y, Random.value * sub.z + startPos.z);
+            Point point = new Point();
+            point.Polar(Random.value * sub.r + inside.r, Random.value * sub.theta + inside.theta);
+            Vector3 pos = new Vector3(point.x, height, point.y);
             // アイテム生成
             GameObject newItem = Object.Instantiate(itemObject[index], pos, Quaternion.identity) as GameObject;
             newItem.transform.parent = transform;
