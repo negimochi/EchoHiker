a6cebe06af19ef2ec670a0f878b4e288d0374a6f
negimochi <negimochi.tabetai@gmail.com>
2012-05-25 09:04:21 +0900

    UI調整、マウスによる回転

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,21 +3,57 @@ using System.Collections;
 
 public class Controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float stepY = 5.0f;
+
+    private GUITexture guiCompass;
+
+	void Start () 
+    {
+        GameObject gameObj = GameObject.Find("/UI/Compass");
+        if (gameObj) { 
+            guiCompass = gameObj.GetComponent<GUITexture>();
+        }
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // 左クリック
-        if (Input.GetMouseButton(0)  )
+	void Update () 
+    {
+        // ドラッグ開始
+        if (Input.GetMouseButtonDown(0))
         {
-            ;
+            Debug.Log("MouseButtonDown");
+        }
+        // ドラッグ中
+        if (Input.GetMouseButton(0) ) {
+            Debug.Log("MouseButton");
+            Rotate( Input.GetAxis("Mouse X") );
+        }
+        // ドラッグ終了
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("MouseButtonUp");
+        }
+
+        if (Input.touchCount > 0)
+        {
+//            if (guiTexture.HitTest())
+            {
+
+            }
         }
 
 //        if( Input.GetAxis("Fire1") ) {
   //          ;
     //    }
 	}
+
+    void Rotate(float movement) 
+    {
+        transform.Rotate(0, movement * stepY, 0);
+        if (guiCompass)
+        {
+            Debug.Log("guiCompass");
+//            guiCompass..transform.Rotate(0, 0, movement * stepY);
+        }
+    }
 }
